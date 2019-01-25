ec09358b794073f16644b1de89f674b1ffd9d4b4
kimber <kimber@sausagemcgriddle.local>
2019-01-25 15:29:24 -0700

    Added Comments

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 def config(app):
-    app.config['SPLUNK_SERVER'] = 'http://localhost:1234'
-    app.config['SPLUNK_USERNAME'] = 'admin'
-    app.config['SPLUNK_PASSWORD'] = 'pw'
-    app.config['SPLUNK_ROLE'] = 'user'
+    app.config['SPLUNK_SERVER'] = 'https://35.247.124.251:8089'
+    app.config['SPLUNK_USERNAME'] = 'ctfd'
+    app.config['SPLUNK_PASSWORD'] = 'p4ssw0rd1'
+    app.config['SPLUNK_ROLE'] = 'admin'
 
