57276ac5c9e6928adaa81f29264334890e041139
kimbermuffin <kimberlyj.duke@gmail.com>
2019-01-25 15:59:00 -0700

    yikes.

--- config.py
+++
@@ -1,6 +0,0 @@
-def config(app):
-    app.config['SPLUNK_SERVER'] = 'https://35.247.124.251:8089'
-    app.config['SPLUNK_USERNAME'] = 'ctfd'
-    app.config['SPLUNK_PASSWORD'] = 'p4ssw0rd1'
-    app.config['SPLUNK_ROLE'] = 'admin'
-
